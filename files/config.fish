0c07bc1798fa6b837096c01570e65b5c9b0c12f8
Nikolay Kiselev <ceo@machineand.me>
2021-08-02 13:18:43 +0000

    Remove icons from nerd font

--- files/config.fish
+++ files/config.fish
@@ -28,17 +28,17 @@ end
 
 function fish_prompt -d "Write out the prompt"
     set laststatus $status
-    printf '\n%s %s%s' (set_color blue) (prompt_pwd) (set_color normal)
+    printf '\n%s%s%s' (set_color blue) (prompt_pwd) (set_color normal)
     if test -d .git
-        printf '\n%s %s%s ' (set_color magenta) (git rev-parse --symbolic-full-name @{upstream} | cut -d "/" -f 3,4 2> /dev/null) (set_color normal)
+        printf '\n%s%s%s ' (set_color magenta) (git rev-parse --symbolic-full-name @{upstream} | cut -d "/" -f 3,4 2> /dev/null) (set_color normal)
     end
-    printf '\n%s %s%s' (set_color green) (date +"%T") (set_color normal)
+    printf '\n%s%s%s' (set_color green) (date +"%T") (set_color normal)
     if test $laststatus -eq 0
-        printf '\n%s%s ' (set_color blue) (set_color normal)
+        printf '\n%s--%s' (set_color blue) (set_color normal)
     else
-        printf '\n%s%s ' (set_color red) (set_color normal)
+        printf '\n%sx-%s' (set_color red) (set_color normal)
     end
-    printf '%s%s ' (set_color normal) (set_color normal)
+    printf '%s>%s ' (set_color normal) (set_color normal)
 end
 
 function summon -d "Get file from master branch"
