f9910e714551f9ec10dff897343a5247fab8e5a8
Nikolay Kiselev <ceo@machineand.me>
2021-08-03 12:12:24 +0000

    Update fish config

--- files/config.fish
+++ files/config.fish
@@ -1,26 +1,23 @@
-# 
 # Functions
-# 
 
 function i
     hostname -I | cut -d " " -f 1
 end
 
 function ll
-    ls -alah
+    ls -alah $argv
 end
 
 function fuck
-    git update-index --assume-unchanged
+    git update-index --assume-unchanged $argv
 end
 
 function unfuck
-    git update-index --no-assume-unchanged
+    git update-index --no-assume-unchanged $argv
 end
 
-#
+
 # Fish-related
-#
 
 function fish_greeting
     printf 'Wellcome %s%s@%s%s\n' (set_color normal) $USER (i) (set_color normal)
@@ -40,6 +37,53 @@ function fish_prompt -d "Write out the prompt"
     end
     printf '%s>%s ' (set_color normal) (set_color normal)
 end
+
+function fish_right_prompt
+    set laststatus $status
+    if test $laststatus -eq 0
+    else
+        printf '%s%s%s' (set_color red) $laststatus (set_color normal)
+    end
+end
+
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t -- $history[1]
+    commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "*!"
+    commandline -f backward-delete-char history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+function fish_user_key_bindings
+  bind ! bind_bang
+  bind '$' bind_dollar
+end
+
+function __task
+    echo trying "\"task $argv\""
+    task $argv
+end
+
+function __fish_default_command_not_found_handler
+    if begin __task $argv; end
+    else
+        printf '%sCommand %s is not found, also as task%s\n' (set_color red) $argv[1] (set_color normal)
+    end
+end
+
+
+# Functions
 
 function summon -d "Get file from master branch"
     git checkout origin/master -- $argv
